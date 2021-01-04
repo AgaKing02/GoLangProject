8c5e5358ef3e1c957e21c78869f116791d67746d
AgakhanPeyishbek <57942020+AgakhanPeyishbek@users.noreply.github.com>
2021-01-04 19:06:40 +0600

    Initial commit(Checking the occurrence of id)

---
+++ app.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+//handler
+func home(responseWriter http.ResponseWriter, request *http.Request)  {
+	if request.URL.Path!="/"{
+		http.NotFound(responseWriter,request)
+		return
+	}
+	responseWriter.Write([]byte("Hello SE 08"))
+}
+func snippetCreate(responseWriter http.ResponseWriter, request *http.Request)  {
+	if request.Method!= http.MethodPost{
+		responseWriter.Header().Set("Allow",http.MethodPost)
+		responseWriter.WriteHeader(http.StatusMethodNotAllowed)
+        responseWriter.Write([]byte("Method not Allowed"))
+		return
+	}
+	responseWriter.Write([]byte("Default snippet"))
+}
+func snippet(responseWriter http.ResponseWriter, request *http.Request)  {
+	id, err := strconv.Atoi(request.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(responseWriter, request)
+		return
+	}
+	fmt.Fprintf(responseWriter,"Display a secific snippet with Id %d...",id)
+}
+func main() {
+	//router
+	mux:=http.NewServeMux()
+	mux.HandleFunc("/",home)
+	mux.HandleFunc("/snippet",snippet)
+	mux.HandleFunc("/snippet/create",snippetCreate)
+
+
+	err:=http.ListenAndServe(":4000",mux)
+
+	log.Fatal(err)
+
+
+}
