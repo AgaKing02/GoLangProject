2d5052cd2342fd4ca72bd302338f3788236d0878
AgakhanPeyishbek <57942020+AgakhanPeyishbek@users.noreply.github.com>
2021-01-10 20:00:55 +0600

    models.go ExampleTransaction

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"database/sql"
 	"errors"
 	"time"
 )
@@ -13,4 +14,28 @@ type Snippet struct {
 	Content string
 	Created time.Time
 	Expires time.Time
+}
+type Example struct {
+	DB *sql.DB
+}
+
+func (m *Example) ExampleTransaction() error {
+	tx, err := m.DB.Begin()
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec("INSERT INTO ...")
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	_, err = tx.Exec("UPDATE  ...")
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	err = tx.Commit()
+	return err
+
 }
