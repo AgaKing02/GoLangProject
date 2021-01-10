98a21b45cc44852cc0b524b2b63dd0aaf1e8a4b7
AgakhanPeyishbek <57942020+AgakhanPeyishbek@users.noreply.github.com>
2021-01-10 19:17:57 +0600

    snippets.go Method Insert

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"awesomeProject/pkg/models/mysql"
+	"database/sql"
 	"flag"
+	"fmt"
+	_ "github.com/lib/pq" // here
 	"log"
 	"net/http"
 	"os"
@@ -10,23 +14,46 @@ import (
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
 }
 
+const (
+	host     = "localhost"
+	port     = 5433
+	user     = "postgres"
+	password = "agahan02"
+	dbname   = "GoLangExamples"
+)
+
 func main() {
+
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+
+	// Initialize a mysql.SnippetModel instance and add it to the application
+	// dependencies.
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  app.routes(), // Call the new app.routes() method
+		Handler:  app.routes(),
 	}
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
---
+++ pkg/models/mysql/snippets.go
@@ -0,0 +1,41 @@
+package mysql
+
+import (
+	"awesomeProject/pkg/models"
+	"database/sql"
+)
+
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+// This will insert a new snippet into the database.
+func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+VALUES(?, ?, CURRENT_TIMESTAMP,
+(CURRENT_TIMESTAMP + INTERVAL '10 DAYS'))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+	// Use the LastInsertId() method on the result object to get the ID of our
+	// newly inserted record in the snippets table.
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	// The ID returned has the type int64, so we convert it to an int type
+	// before returning.
+	return int(id), nil
+}
+
+// This will return a specific snippet based on its id.
+func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
+	return nil, nil
+}
+
+// This will return the 10 most recently created snippets.
+func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
+	return nil, nil
+}
