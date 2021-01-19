b0ddb18d1af5d32cf5dea4dbae6e42c5276ebe95
AgakhanPeyishbek <57942020+AgakhanPeyishbek@users.noreply.github.com>
2021-01-19 12:25:00 +0600

    Using pgx instead of DB

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -2,45 +2,35 @@ package mysql
 
 import (
 	"awesomeProject/pkg/models"
+	"context"
 	"database/sql"
 	"errors"
+	"github.com/jackc/pgx/pgxpool"
+	"strconv"
+	"time"
 )
 
 type SnippetModel struct {
-	DB *sql.DB
+	Pool *pgxpool.Pool
 }
 
 // This will insert a new snippet into the database.
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	stmt := `INSERT INTO snippets (title, content, created, expires)
-VALUES($1, $2, CURRENT_TIMESTAMP,
-(CURRENT_TIMESTAMP + INTERVAL '10 DAYS'))`
-
-	result, err := m.DB.Exec(stmt, title, content, expires)
+	var id uint64
+	interval, err := strconv.Atoi(expires)
+	row := m.Pool.QueryRow(context.Background(), "INSERT INTO snippets (title,content,created,expires) VALUES ($1,$2,$3,$4) RETURNING id", title, content, time.Now(), time.Now().AddDate(0, 0, interval))
+	err = row.Scan(&id)
 	if err != nil {
 		return 0, err
 	}
-	// Use the LastInsertId() method on the result object to get the ID of our
-	// newly inserted record in the snippets table.
-	id, err := result.LastInsertId()
-	if err != nil {
-		return 0, err
-	}
-	// The ID returned has the type int64, so we convert it to an int type
-	// before returning.
 	return int(id), nil
 }
 
 // This will return a specific snippet based on its id.
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	stmt := `SELECT id, title, content, created, expires FROM snippets
-WHERE expires > CURRENT_TIMESTAMP AND id = $1`
-
-	row := m.DB.QueryRow(stmt, id)
-
 	s := &models.Snippet{}
-
-	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	err := m.Pool.QueryRow(context.Background(), "SELECT id, title, content, created, expires FROM snippets where id=$1 AND expires > now()", id).
+		Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, models.ErrNoRecord
@@ -53,30 +43,22 @@ WHERE expires > CURRENT_TIMESTAMP AND id = $1`
 
 // This will return the 10 most recently created snippets.
 func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
-	stmt := `SELECT id, title, content, created, expires FROM snippets
-WHERE expires > CURRENT_TIMESTAMP ORDER BY created DESC LIMIT 10`
-
-	rows, err := m.DB.Query(stmt)
+	rows, err := m.Pool.Query(context.Background(), "SELECT id, title, content, created, expires FROM snippets WHERE expires > now() ORDER BY created DESC LIMIT 10")
 	if err != nil {
 		return nil, err
 	}
 
 	defer rows.Close()
-
-	snippets := []*models.Snippet{}
+	var snippets []*models.Snippet
 
 	for rows.Next() {
-
 		s := &models.Snippet{}
-
 		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 		if err != nil {
 			return nil, err
 		}
-
 		snippets = append(snippets, s)
 	}
-
 	if err = rows.Err(); err != nil {
 		return nil, err
 	}
